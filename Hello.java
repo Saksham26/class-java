253de3b7a4865716ac0c556aba9f7789f042f5ef
Saksham26 <41563468+Saksham26@users.noreply.github.com>
2018-08-07 13:19:55 +0530

    Add files via upload
    CLI args

---
+++ Hello.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class Hello
+
+{
+
+	public static void main(String[] args)
+	{
+
+
+
+	for(int i=0;i<args.length;i++)
+	{
+	
+			System.out.println(args[i]);
+	}
+/*		if (args.length > 0)
+
+		{
+			
+System.out.println("The command line"+" arguments are:");
+
+
+			 iterating the args array and printing
+
+			 the command line arguments
+
+			for (String val:args)
+
+				System.out.println(val);
+
+		}
+		
+else
+
+System.out.println("No command line "+"arguments found.");
+
+*/
+
+
+}
+
+}
